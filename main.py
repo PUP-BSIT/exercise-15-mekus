252588cf74c8055279f575a40d9a1a02a896e751
jundam-dannkyle <jundamdannkyle@gmail.com>
2025-06-11 23:24:47 +0800

    Feature/jundam main (#27)
    * clean up mekus imports into a single line
    
    * add jundam module implementation to main.py

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 import os
+
 import pyfiglet
-from mekus import PokemonGame, RockPaperScissors, MobileLegendsStats, NBA2K25
+
+from mekus import (
+    PokemonGame, 
+    RockPaperScissors, 
+    MobileLegendsStats, 
+    NBA2K25,
+    ValorantOps
+)
 
 EXIT_CHOICE = "0"
 MENU_CHOICES = {
@@ -41,8 +49,7 @@ class MekusModules:
 
     def run_jundam_module(self):
         """Run Jundam's module."""
-        # TODO(Jundam): Implement integration
-        pass
+        ValorantOps().menu()
 
     def run_olazo_module(self):
         """Run Olazo's module."""
